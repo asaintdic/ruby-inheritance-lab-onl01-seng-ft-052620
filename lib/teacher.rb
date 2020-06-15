4d83984d62c85a6c2742411e93b3dc113b4d706d
Alain Saint-Dic <alains214@gmail.com>
2020-06-15 06:04:57 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -2,9 +2,14 @@ require_relative "./user.rb"
 class Teacher < User
    
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+<<<<<<< HEAD
   def initialize
   end
   def teach
     KNOWLEDGE.sample
   end
+=======
+
+  def teach
+>>>>>>> 87395e42f70d7d9b585717c4af1486e18f554595
 end
--- lib/user.rb
+++ lib/user.rb
@@ -1,13 +1,23 @@
 class User 
   
   attr_accessor :first_name, :last_name
+<<<<<<< HEAD
  
   
   def initialize(first_name = nil, last_name = nil)
+=======
+  
+  def initialize(first_name, last_name)
+>>>>>>> 87395e42f70d7d9b585717c4af1486e18f554595
     @first_name = first_name
     @last_name = last_name
   end 
   
+<<<<<<< HEAD
  
+=======
+  
+    
+>>>>>>> 87395e42f70d7d9b585717c4af1486e18f554595
     
 end 
